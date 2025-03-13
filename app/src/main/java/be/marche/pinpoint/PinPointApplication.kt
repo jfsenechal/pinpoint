8f1dc1d38aa151162ce20241017199bc3cc9a4a4
jfsenechal <jfsenechal@gmail.com>
2025-03-13 13:01:03 +0100

    service location

--- app/src/main/java/be/marche/pinpoint/PinPointApplication.kt
+++ app/src/main/java/be/marche/pinpoint/PinPointApplication.kt
@@ -1,12 +1,25 @@
 package be.marche.pinpoint
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import be.marche.pinpoint.geolocation.LocationService
 
-class PinPointApplication: Application() {
+class PinPointApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        val channel = NotificationChannel(
+            LocationService.LOCATION_CHANNEL,
+            "Location",
+            NotificationManager.IMPORTANCE_LOW
 
+        )
+
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
     }
 }
